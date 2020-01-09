f8680c6df1444ec912466790585c6ad4161273ff
KrashMunoz <krash.munoz@gmail.com>
2020-01-09 02:28:24 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {  FormGroup, FormControl } from '@angular/forms';
+import { RegistrationService } from './registration.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'student';
+  title: string = 'Student Widget';
+  studentForm = new FormGroup ({
+    firstName : new FormControl(''),
+    lastName : new FormControl(''),
+  });
+
+  constructor(private _registrationService: RegistrationService){}
+  // updateName($event) {
+  //   this.studentForm.firstName.setValue($event);
+  // }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.studentForm.value);
+
+    this._registrationService.registerStudent(this.studentForm) 
+      .subscribe(
+        data => console.log('Success!', data),
+        error => console.log('Error!', error)
+      )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from './app-material/app-material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppMaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
